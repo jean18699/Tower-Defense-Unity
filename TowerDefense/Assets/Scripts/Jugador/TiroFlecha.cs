400a7950c548a89fb93e43c9dcd3b1c257d09ea1
Jean Carlos <jean18699@gmail.com>
2019-03-01 17:00:44 -0400

    tiroFlecha script

---
+++ TowerDefense/Assets/Scripts/Jugador/TiroFlecha.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroFlecha : MonoBehaviour
+{
+
+    Vector3 cambioPosicion = Vector3.zero;
+    Vector3 velocidadFinal = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cambioPosicion.x = velocidadFinal.x * Time.deltaTime + (Physics.gravity.x * (Mathf.Pow(Time.deltaTime, 2f) / 2));
+
+        cambioPosicion.y = velocidadFinal.y * Time.deltaTime + (Physics.gravity.y * (Mathf.Pow(Time.deltaTime, 2f) / 2));
+
+        gameObject.transform.Translate(cambioPosicion);
+
+        velocidadFinal += Physics.gravity * Time.deltaTime;
+    }
+}
