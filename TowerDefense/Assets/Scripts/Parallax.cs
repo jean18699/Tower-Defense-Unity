27789741d3b48f65b6ccf2883c3f677a739366c3
Jean Carlos <jean18699@gmail.com>
2019-03-01 16:00:47 -0400

    Efecto Parallax
    Si no lo bajas te mato

---
+++ TowerDefense/Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float velocidadImagen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(velocidadImagen, 0));
+        if (transform.position.x >= 49.1f)
+            transform.position = new Vector3(-49f, 0, 0);
+    }
+}
