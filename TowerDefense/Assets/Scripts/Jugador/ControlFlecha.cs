ff49299e3fad66bb97e26c2ddd17f8e1c5e59d05
jean18699 <jean18699@gmail.com>
2019-03-02 02:25:24 -0400

    Revert "Intento tiro parabola y rotacion"
    This reverts commit 8fdf822fa2c14d8f96e5e7d393380fa00362b36e.

--- TowerDefense/Assets/Scripts/Jugador/ControlFlecha.cs
+++ TowerDefense/Assets/Scripts/Jugador/ControlFlecha.cs
@@ -7,7 +7,7 @@ public class ControlFlecha : MonoBehaviour
 {
     Global scrGlobales;
 	public float Gravity = -9.8f;
-	float Velocidad = 20;
+	float Velocidad = 15;
 	public Vector3 mousePos;
 	float duracionDeVuelo;
 	float angulo;
@@ -22,10 +22,6 @@ public class ControlFlecha : MonoBehaviour
 	float X;
 	float Y;
 
-	float yAnterior;
-	float xAnterior;
-	float anguloRotacion;
-
 	// Start is called before the first frame update
 	void Start()
     {
@@ -35,8 +31,8 @@ public class ControlFlecha : MonoBehaviour
 		VoY = Velocidad * Mathf.Sin(angulo);
 		duracionDeVuelo = Mathf.Abs(transform.parent.position.x / Vx);
 
-		Xo = transform.parent.position.x;
-		Yo = transform.parent.position.y + transform.parent.localScale.y;
+		Xo = transform.position.x;
+		Yo = transform.position.y;
 	}
 
     // Update is called once per frame
@@ -56,6 +52,8 @@ public class ControlFlecha : MonoBehaviour
 		
 		transform.SetParent(null);
 
+		print(angulo);
+
 		Stopwatch stopWatch = new Stopwatch();
 		stopWatch.Start();
 
@@ -67,30 +65,16 @@ public class ControlFlecha : MonoBehaviour
 
 			elapse_time += Time.deltaTime;
 
-			
-
-			Vy = VoY - Gravity * elapse_time;
+			Vy = VoY - Gravity * Time.deltaTime;
 
 			X = Xo + Vx * elapse_time;
 			Y = Yo + VoY - (Gravity / 2) * Mathf.Pow(elapse_time, 2);
 
-			
-			
-			transform.localRotation = Quaternion.Euler(0, 0, (yAnterior + Y));
-			//print(yAnterior + " " + Y);
-			yAnterior = Y;
-			xAnterior = X;
-
-			float xTriangulo = xAnterior - X;
-			float yTriangulo = yAnterior - Y;
-			anguloRotacion = Mathf.Atan2(xTriangulo, yTriangulo) * Mathf.Rad2Deg;
-			print(xAnterior + " " + X);
-
-
 			transform.position = new Vector2(X, Y);
-			
 
 			//transform.Translate(Xo + Vx * elapse_time, (VoY - (Gravity * elapse_time)) * Time.deltaTime, 0);
+
+			UnityEngine.Debug.Log(Xo);
 
 		}
 	}
