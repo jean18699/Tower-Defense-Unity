6a51b47f430563f411b37c9444fe05f0f4a13444
hardow2011 <45014033+hardow2011@users.noreply.github.com>
2019-03-03 17:19:48 -0400

    tiro parab mejorado

--- TowerDefense/Assets/Scripts/Jugador/ControlArco.cs
+++ TowerDefense/Assets/Scripts/Jugador/ControlArco.cs
@@ -55,10 +55,10 @@ public class ControlArco : MonoBehaviour
 			//var _flechaScript = GameObject.FindGameObjectWithTag("Flecha").GetComponent<ControlFlecha>();
 			//_flechaScript.mousePos = mousePos;
 			var _flechaScript = _flecha.GetComponent<ControlFlecha>();
-			_flechaScript.mousePos = mousePos;
-			_flecha.transform.position = new Vector2(0, 0);
-			_flecha.transform.SetParent(gameObject.transform);
-			_flecha.transform.localPosition = new Vector2(0, 0);
+			_flechaScript.angulo = angle;
+			//_flecha.transform.position = new Vector2(0, 0);
+			//_flecha.transform.SetParent(gameObject.transform);
+			_flechaScript.PosicionFlecha = transform.position;
 			//_flecha.transform.SetParent(null);
 
 			//_flecha.transform.SetParent(null);
